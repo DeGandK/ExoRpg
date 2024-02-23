16bfabbb1c833b703dba5ce2d04c229e9827d920
Kévin De Gand <degandk@gmail.com>
2024-02-23 15:56:38 +0100

    PremierCommit

---
+++ ExoRPG/MesClasses/Pretre.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExoRPG.MesClasses
+{
+    internal class Pretre
+    {
+        public string Nom {  get; set; }
+
+        private int _force;
+        public int Force 
+        { 
+            get { return _force; } 
+
+            private set { _force = value; } 
+        }
+
+        private int _endurance;
+        public int Endurance
+        {
+            get { return _endurance; }
+
+            private set { _endurance = value; }
+        }
+
+        private int _intelligence;
+        public int Intelligence
+        {
+            get { return _intelligence +2; }
+
+            private set { _intelligence = value; }
+        }
+
+        private int _sagesse;
+        public int Sagesse
+        {
+            get { return _sagesse +4; }
+
+            private set { _sagesse = value; }
+        }
+
+        public void GenererCarac()
+        {
+            Random random = new Random();
+
+            _force = random.Next(10, 21);
+            _endurance = random.Next(10, 21);
+            _intelligence = random.Next(10, 21);
+            _sagesse = random.Next(10, 21);
+        }
+    }
+}
---
+++ ExoRPG/Program.cs
@@ -0,0 +1,33 @@
+﻿using ExoRPG.MesClasses;
+
+namespace ExoRPG
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            MesClasses.Guerrier Garrosh  = new Guerrier();
+            
+            Garrosh.GenererCarac();
+            Garrosh.Nom = "Garrosh";
+            Console.WriteLine(Garrosh.Force);
+            Console.WriteLine(Garrosh.Nom);
+
+            MesClasses.Pretre Velen = new Pretre();
+            Velen.GenererCarac();
+            Velen.Nom = "Velen";
+            Console.WriteLine(Velen.Force);
+            Console.WriteLine(Velen.Nom);
+
+
+
+
+
+      
+
+
+        }
+    }
+}
